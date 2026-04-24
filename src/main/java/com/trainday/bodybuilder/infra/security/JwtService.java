17215f448f9b45f6cd9f64bba4538918e189268c
Danielpernnasc <dpericles6@gmail.com>
2026-04-24 18:15:42 -0300

    atualizando

--- src/main/java/com/trainday/bodybuilder/infra/security/JwtService.java
+++ src/main/java/com/trainday/bodybuilder/infra/security/JwtService.java
@@ -2,7 +2,7 @@ package com.trainday.bodybuilder.infra.security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
-///import jakarta.annotation.PostConstruct;
+// import jakarta.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
