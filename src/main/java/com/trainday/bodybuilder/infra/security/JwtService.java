328eda96f094f7499f86ef9d69c4969c2915530f
Daniel Nascimento <nascimento@NascimentoDan.localdomain>
2026-05-01 16:25:10 -0300

    primeiro commit

--- src/main/java/com/trainday/bodybuilder/infra/security/JwtService.java
+++ src/main/java/com/trainday/bodybuilder/infra/security/JwtService.java
@@ -2,7 +2,7 @@ package com.trainday.bodybuilder.infra.security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
-// import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -15,10 +15,10 @@ import java.util.Map;
 @Service
 public class JwtService{
 
-    // @PostConstruct
-    // public void init() {
-    //     System.out.println("JWT SECRET 8080 = [" + secret + "]");
-    // }
+    @PostConstruct
+    public void init() {
+        System.out.println("JWT SECRET 8080 = [" + secret + "]");
+    }
 
     @Value("${jwt.secret}")
     private String secret;
