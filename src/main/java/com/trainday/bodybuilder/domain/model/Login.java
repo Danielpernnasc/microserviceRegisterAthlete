f4d454f34bbce44462c210153d765342114596f9
Daniel <dpericles6@gmail.com>
2026-05-06 17:44:41 -0300

    Update Login.java

--- src/main/java/com/trainday/bodybuilder/domain/model/Login.java
+++ src/main/java/com/trainday/bodybuilder/domain/model/Login.java
@@ -21,35 +21,4 @@ public class Login {
     private String email; // usar o email da variavel que vem do Athlete
     private String password;
 
-    // public Login(){}
-
-    // public Login(String email, String password){
-    //     this.id = id;
-    //     this.email = email;
-    //     this.password = password;
-    // }
-
-    // public String getId(){
-    //     return id;
-    // }
-
-    // public void setId(String id){
-    //     this.id = id;
-    // }
-
-    // public String getEmail(){
-    //     return email;
-    // }
-
-    // public void setEmail(String email){
-    //     this.email = email;
-    // }
-
-    // public String getPassword(){
-    //     return password;
-    // }
-
-    // public void setPassword(String password){
-    //     this.password = password;
-    // }
 }
